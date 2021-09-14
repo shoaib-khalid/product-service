ad19849f1087d989fa3a9de8b41a92a221afcbdb
sarosh <sarosh.tariq@kalsym.com>
2021-09-14 17:47:04 +0500

    fixed dicsount not returning option

--- src/main/java/com/kalsym/product/service/controller/StoreDiscountController.java
+++ src/main/java/com/kalsym/product/service/controller/StoreDiscountController.java
@@ -90,10 +90,11 @@ public class StoreDiscountController {
             response.setStatus(HttpStatus.NOT_FOUND);
             return ResponseEntity.status(response.getStatus()).body(response);
         }
-                
+
         Logger.application.info(Logger.pattern, ProductServiceApplication.VERSION, logprefix, "Store Object:" + optStore);
         storeDiscount.setStoreId(storeId);
-        storeDiscountRepository.save(storeDiscount);
+
+        response.setData(storeDiscountRepository.save(storeDiscount));
         response.setStatus(HttpStatus.CREATED);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
@@ -114,7 +115,7 @@ public class StoreDiscountController {
             response.setStatus(HttpStatus.NOT_FOUND);
             return ResponseEntity.status(response.getStatus()).body(response);
         }
-        
+
         Optional<StoreDiscount> optStoreDiscount = storeDiscountRepository.findById(storeDiscount.getId());
 
         if (!optStoreDiscount.isPresent()) {
@@ -125,7 +126,8 @@ public class StoreDiscountController {
 
         Logger.application.info(Logger.pattern, ProductServiceApplication.VERSION, logprefix, "StoreDiscount Found Object:" + optStoreDiscount);
         storeDiscount.setStoreId(storeId);
-        storeDiscountRepository.save(storeDiscount);
+        response.setData(storeDiscountRepository.save(storeDiscount));
+
         response.setStatus(HttpStatus.OK);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
