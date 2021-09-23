4aa9329d1b4eb0767d563c6883dfa51fa3f7c4da
taufik <taufik@kalsym.com>
2021-09-23 12:57:13 +0800

    add checking overlap discount & tier

--- src/main/java/com/kalsym/product/service/repository/StoreDiscountRepository.java
+++ src/main/java/com/kalsym/product/service/repository/StoreDiscountRepository.java
@@ -46,7 +46,7 @@ public interface StoreDiscountRepository
             );
     
         @Query("SELECT m FROM StoreDiscount m WHERE m.storeId = :queryStoreId "
-                + "AND m.isActive=true "
+                + "AND m.isActive=true AND m.discountType = :discountType "
                 + "AND ("
                 + "(startDateTime >= m.startDate AND startDateTime <= m.endDate) "
                 + " OR (endDateTime >= m.startDate AND endDateTime <= m.endDate)"
@@ -59,7 +59,7 @@ public interface StoreDiscountRepository
             );
     
     @Query("SELECT m FROM StoreDiscount m WHERE m.storeId = :queryStoreId "
-                + "AND m.isActive=true AND m.id <> :discountId "
+                + "AND m.isActive=true AND m.id <> :discountId AND m.discountType = :discountType "
                 + "AND ("
                 + "(startDateTime >= m.startDate AND startDateTime <= m.endDate) "
                 + " OR (endDateTime >= m.startDate AND endDateTime <= m.endDate)"
