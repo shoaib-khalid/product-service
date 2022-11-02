d3a6624209a01733741be984d7c8f5f1d24613c2
ImanNazirah <nurimannazirah95@gmail.com>
2022-11-02 10:01:49 +0800

    test code

--- src/main/java/com/kalsym/product/service/controller/CodePlaygroundController.java
+++ src/main/java/com/kalsym/product/service/controller/CodePlaygroundController.java
@@ -3,6 +3,7 @@ package com.kalsym.product.service.controller;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,16 +13,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kalsym.product.service.model.product.AddOnTemplateGroup;
 import com.kalsym.product.service.model.product.AddOnTemplateItem;
+import com.kalsym.product.service.model.product.ProductAddOn;
+import com.kalsym.product.service.model.product.ProductAddOnGroup;
 import com.kalsym.product.service.model.store.CompareStoreTemplateGroup;
 import com.kalsym.product.service.model.store.CompareStoreTemplateItem;
 import com.kalsym.product.service.repository.AddOnTemplateGroupRepository;
 import com.kalsym.product.service.repository.AddOnTemplateItemRepository;
+import com.kalsym.product.service.repository.ProductAddOnGroupRepository;
+import com.kalsym.product.service.repository.ProductAddOnRepository;
 import com.kalsym.product.service.utility.HttpResponse;
 
 @RestController()
@@ -33,6 +40,12 @@ public class CodePlaygroundController {
 
     @Autowired
     AddOnTemplateItemRepository addOnTemplateItemRepository;
+
+    @Autowired 
+    ProductAddOnRepository productAddOnRepository;
+
+    @Autowired 
+    ProductAddOnGroupRepository productAddOnGroupRepository;
 
     @GetMapping(path = {""}, name = "store-categories-get", produces = "application/json")
     @PreAuthorize("hasAnyAuthority('store-categories-get', 'all')")
@@ -160,6 +173,93 @@ public class CodePlaygroundController {
             response.setData(filterDataTemplateGroup);
             return ResponseEntity.status(response.getStatus()).body(response);
             
+        } catch (Exception e) {
+            // TODO: handle exception
+
+            e.printStackTrace();            
+            response.setStatus(HttpStatus.BAD_REQUEST);
+            response.setError(e.toString());
+            return ResponseEntity.status(response.getStatus()).body(response);
+        }
+
+
+    }
+
+    @PostMapping(path = {"/try"}, name = "store-categories-get", produces = "application/json")
+    @PreAuthorize("hasAnyAuthority('store-categories-get', 'all')")
+    public ResponseEntity<HttpResponse> getSelectedProduct(HttpServletRequest request,
+            @RequestParam(required = true) String storeOwnerId,
+            @RequestParam(required = true) String storeBranchId,
+            @RequestBody List<String> productIds
+            ) {
+
+        HttpResponse response = new HttpResponse(request.getRequestURI());
+
+        try {
+
+            List<AddOnTemplateGroup> storeOwnerTemplateGroup = addOnTemplateGroupRepository.findByStoreIdAndStatusNot(storeOwnerId, "DELETED");
+
+            List<AddOnTemplateGroup> storeBranchTemplateGroup = addOnTemplateGroupRepository.findByStoreIdAndStatusNot(storeBranchId, "DELETED");
+
+            List<CompareStoreTemplateGroup> compareStoreOwnerTemplateGroup = new ArrayList<>();
+
+            storeOwnerTemplateGroup.stream()
+            .map((AddOnTemplateGroup x)->{
+
+                Optional<AddOnTemplateGroup> optFilterStoreBranchAddOnTemplateGroup = storeBranchTemplateGroup.stream()
+                .filter((AddOnTemplateGroup atg) -> atg.getTitle().contains(x.getTitle()))
+                .findFirst();
+
+                CompareStoreTemplateGroup compareTemplateGroup = new CompareStoreTemplateGroup();
+                compareTemplateGroup.setStoreTemplateGroupId(x.getId());
+                compareTemplateGroup.setTitle(x.getTitle());
+                compareTemplateGroup.setBranchTemplateGroupId(optFilterStoreBranchAddOnTemplateGroup.isPresent()?optFilterStoreBranchAddOnTemplateGroup.get().getId():null);
+
+                List<CompareStoreTemplateItem> compareStoreOwnerTemplateItem = new ArrayList<>();
+
+                x.getAddOnTemplateItem().stream()
+                .map( (AddOnTemplateItem ownerTemplateItem)->{
+
+                    CompareStoreTemplateItem compareDataTemplateItem = new CompareStoreTemplateItem();
+                    compareDataTemplateItem.setStoreTemplateItem(ownerTemplateItem.getId());
+                    compareDataTemplateItem.setName(ownerTemplateItem.getName());
+
+
+                    if(optFilterStoreBranchAddOnTemplateGroup.isPresent()){
+                        Optional<AddOnTemplateItem> optFilterStoreBranchTemplateItem = optFilterStoreBranchAddOnTemplateGroup
+                        .get().getAddOnTemplateItem()
+                        .stream()
+                        .filter((AddOnTemplateItem ati) -> ati.getName().contains(ownerTemplateItem.getName()))
+                        .findFirst();
+
+                        compareDataTemplateItem.setBranchTemplateItem(optFilterStoreBranchTemplateItem.isPresent()?optFilterStoreBranchTemplateItem.get().getId():null);
+
+
+                    } else{
+                        compareDataTemplateItem.setBranchTemplateItem(null);
+                    }
+               
+
+                  
+                    compareStoreOwnerTemplateItem.add(compareDataTemplateItem);
+
+                    return ownerTemplateItem;
+                })
+                .collect(Collectors.toList());
+
+                compareTemplateGroup.setCompareTemplateItem(compareStoreOwnerTemplateItem);
+
+                compareStoreOwnerTemplateGroup.add(compareTemplateGroup);
+                
+                System.out.println("compareStoreOwnerTemplateGroup jeeeee"+compareStoreOwnerTemplateGroup);
+
+                return x;
+            })
+            .collect(Collectors.toList());
+
+            response.setStatus(HttpStatus.OK);
+            response.setData(compareStoreOwnerTemplateGroup);
+            return ResponseEntity.status(response.getStatus()).body(response); 
         } catch (Exception e) {
             // TODO: handle exception
 
